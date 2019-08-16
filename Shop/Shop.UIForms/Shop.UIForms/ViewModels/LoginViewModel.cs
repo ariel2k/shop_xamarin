0b4c7db92ad20f10aaf7ebec155eee212bcd184f
Ariel Molina <arielhmolina@gmail.com>
2019-08-15 22:23:29 -0300

    Consume Token From App

--- Shop/Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop/Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -1,41 +1,89 @@
 ﻿namespace Shop.UIForms.ViewModels
 {
+    using Common.Services;
     using GalaSoft.MvvmLight.Command;
-    using Shop.UIForms.Views;
-    using System;
+    using Shop.Common.Models;
     using System.Windows.Input;
+    using Views;
     using Xamarin.Forms;
 
-    public class LoginViewModel 
+    public class LoginViewModel : BaseViewModel
     {
+        private bool isRunning;
+        private bool isEnabled;
+        private readonly ApiService apiService;
+
+        public bool IsRunning
+        {
+            get => this.isRunning;
+            set => this.SetValue(ref this.isRunning, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => this.isEnabled;
+            set => this.SetValue(ref this.isEnabled, value);
+        }
+
         public string Email { get; set; }
 
         public string Password { get; set; }
 
-        public ICommand LoginCommand => new RelayCommand(Login);
+        public ICommand LoginCommand => new RelayCommand(this.Login);
+
+        public LoginViewModel()
+        {
+            this.apiService = new ApiService();
+            this.IsEnabled = true;
+            this.Email = "jzuluaga55@gmail.com";
+            this.Password = "123456";
+        }
 
         private async void Login()
         {
-            if (String.IsNullOrEmpty(Email))
+            if (string.IsNullOrEmpty(this.Email))
             {
-                await Application.Current.MainPage.DisplayAlert(
-                    "Error",
-                    "You must enter a email",
-                    "Accept");
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter an email.", "Accept");
                 return;
             }
 
-            if (String.IsNullOrEmpty(Password))
+            if (string.IsNullOrEmpty(this.Password))
             {
-                await Application.Current.MainPage.DisplayAlert(
-                    "Error",
-                    "You must enter a password",
-                    "Accept");
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter a password.", "Accept");
                 return;
             }
 
-            MainViewModel.GetInstance().Products = new ProductsViewModel();
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            var request = new TokenRequest
+            {
+                Password = this.Password,
+                Username = this.Email
+            };
+
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var response = await this.apiService.GetTokenAsync(
+                url,
+                "Shop/Account",
+                "/CreateToken",
+                request);
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Email or password incorrect.", "Accept");
+                return;
+            }
+
+            var token = (TokenResponse)response.Result;
+            var mainViewModel = MainViewModel.GetInstance();
+            mainViewModel.Token = token;
+            mainViewModel.Products = new ProductsViewModel();
             await Application.Current.MainPage.Navigation.PushAsync(new ProductsPage());
         }
     }
+
 }
